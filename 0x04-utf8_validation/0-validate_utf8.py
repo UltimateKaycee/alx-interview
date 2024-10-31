a1fe9303abea275217921f70241a4dcc2cd42f8b
Ultimate Kaycee <ultimatekaycee@outlook.com>
2024-10-31 05:12:14 +0000

    redo validation task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pythoni3
+#!/usr/bin/python3
 """
 Module for UTF-8 Validation
 """
@@ -6,25 +6,24 @@ Module for UTF-8 Validation
 
 def validUTF8(data):
     """
-    Function Definition
-    Data: integer lists
-    Return: Will return True if data is a valid UTF-8
-    encoding, otherwise will return false
+    Data: list of int
+    Return: If data is a valid UTF-8 encoding then TRUE
+    otherwise FALSE
     """
-    countbyte = 0
+    byte_count = 0
 
-    for bain in data:
-        if countbyte == 0:
-            if bain >> 5 == 0b110 or bain >> 5 == 0b1110:
-                countbyte = 1
-            elif bain >> 4 == 0b1110:
-                countbyte = 2
-            elif bain >> 3 == 0b11110:
-                countbyte = 3
-            elif bain >> 7 == 0b1:
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
                 return False
         else:
-            if bain >> 6 != 0b10:
+            if i >> 6 != 0b10:
                 return False
-            countbyte -= 1
-    return countbyte == 0
+            byte_count -= 1
+    return byte_count == 0
